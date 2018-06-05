6827af6b2d15e0e2b9ece15dee160e610bab9bb0
Paul Brower <pabrower@icloud.com>
2018-06-05 13:48:57 -0700

    completed checkpoint blocJams Playback

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,9 +11,39 @@ class Album extends Component {
      	});
  
      this.state = {
-       album: album
+       album: album,
+       currentSong: album.songs[0],
+       isPlaying: false
      };
+
+     this.audioElement = document.createELement('audio');
+     this.audioElement = album.songs[0].audioSrc;
 	}
+
+	play() {
+     this.audioElement.play();
+     this.setState({ isPlaying: true });
+   }
+
+   pause() {
+     this.audioElement.pause();
+     this.setState({ isPlaying: false });
+   }   
+
+   setSong(song) {
+     this.audioElement.src = song.audioSrc;
+     this.setState({ currentSong: song });
+   }
+
+   handleSongClick(song) {
+     const isSameSong = this.state.currentSong === song;
+     if (this.state.isPlaying && isSameSong) {
+       this.pause();
+     } else {
+       if (!isSameSong) { this.setSong(song); }
+       this.play();
+     }
+   }
 
 	render() {
 		return (
@@ -33,19 +63,17 @@ class Album extends Component {
 	             <col id="song-duration-column" />
 	           </colgroup>  
 	           <tbody>
-	           {
-	           		this.state.album.songs.map( (song, index, album) =>
+	           		{this.state.album.songs.map( (song, index, album) =>
 	           			<tr key={index}>
 	           				<td>{index +1}</td>
 	           				<td>{song.title}</td>
 	           				<td>{song.duration}</td>
 	           			</tr>
-	           		)
-	           }
+	           		)}
 	           </tbody>
-         </table>
-			</section>
-  );
+         	</table>
+		</section>
+  	);
  }
 }
 
