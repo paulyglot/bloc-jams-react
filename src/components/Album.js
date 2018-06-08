970465863ebb38b30273558e593cbf737cfc7b78
Paul Brower <pabrower@icloud.com>
2018-06-08 11:55:05 -0700

    made updates to hover functionality in master, updating to branch

--- src/components/Album.js
+++ src/components/Album.js
@@ -5,8 +5,8 @@ class Album extends Component {
 	constructor(props) {
 		super(props);
 
-		const album = albumData.find( album => {
-       		return album.slug === this.props.match.params.slug
+			const album = albumData.find( album => {
+   				return album.slug === this.props.match.params.slug
      	});
  
      this.state = {
@@ -16,7 +16,7 @@ class Album extends Component {
      };
 
      this.audioElement = document.createElement('audio');
-     this.audioElement.src = album.songs[0].audioSrc;
+     this.audioElement = album.songs[0].audioSrc;
 	}
 
 	play() {
@@ -24,17 +24,17 @@ class Album extends Component {
      this.setState({ isPlaying: true });
    }
 
-   	pause() {
+    pause() {
      this.audioElement.pause();
      this.setState({ isPlaying: false });
-   }
+   }   
 
-   	setSong(song) {
+    setSong(song) {
      this.audioElement.src = song.audioSrc;
      this.setState({ currentSong: song });
    }
 
-   	handleSongClick(song) {
+    handleSongClick(song) {
      const isSameSong = this.state.currentSong === song;
      if (this.state.isPlaying && isSameSong) {
        this.pause();
@@ -44,40 +44,63 @@ class Album extends Component {
      }
    }
 
+   //When I hover over a song, it displays a "play" button in place of the song number.
 
+   //The currently playing song displays a "pause" button in place of the song number.
 
+   //A paused song displays a "play" button in place of the song number.
+
+    buttonController(song, index) {
+	    const playButton = <span className="ion-play"></span>;
+	    const pauseButton = <span className="ion-pause"></span>;
+	    const isSameSong = this.state.currentSong === song;    
+
+	    if (this.state.isPlaying === false && this.state.hovered === song)  {
+	      return playButton;
+	    } else if (this.state.isPlaying === true && this.state.hovered === song && isSameSong) {
+	      return pauseButton;
+	    } else {
+	      return (index +1);
+	    }
+	  }     
+
+    mouseHoverOn(song) {
+    	this.setState({hovered: song});
+    }
+
+    mouseHoverOff(song) {
+    	this.setState({hovered: null});
+    }
+    
 	render() {
 		return (
 			<section className="album">
-				<section id="album-info">
-	        		<img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
-	        		<div className="album-details">
-		    			<h1 id="album-title">{this.state.album.title}</h1>
-		    			<h2 className="artist">{this.state.album.artist}</h2>
-	        			<div id="release-info">{this.state.album.releaseInfo}</div>  
-	        		</div>
-	        	</section>
-	        		<table id="song-list">
-	        			<colgroup>
-					        <col id="song-number-column" />
-					        <col id="song-title-column" />
-					        <col id="song-duration-column" />
-	        			</colgroup>  
-	        			<tbody>
-	        				{
-	        				this.state.album.songs.map((song, index, album) =>
-	        					<tr className="song" to key={index} onClick={() => this.handleSongClick(song)}>
-	        						<td>{index +1}</td>
-	        						<td>{song.title}</td>
-	        						<td>{song.duration}</td>
-	        					</tr>
-	        					)
-	        				}
-	        			</tbody>
-           			</table> 
-	        	</section>
-  				);
- 			}
-		}
-
+			  <section id="album-info">
+	           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+	           <div className="album-details">
+	           <h1 id="album-title">{this.state.album.title}</h1>
+	           <h2 className="artist">{this.state.album.artist}</h2>
+	           <div id="release-info">{this.state.album.releaseInfo}</div>  
+	           </div>
+	         </section> 
+	         <table id="song-list">
+	           <colgroup>
+	             <col id="song-number-column" />
+	             <col id="song-title-column" />
+	             <col id="song-duration-column" />
+	           </colgroup>  
+	           <tbody>
+	           		{this.state.album.songs.map( (song,index) =>
+	           			<tr key={index} onClick={() => this.handleSongClick(song)} onMouseEnter={() => this.mouseHoverOn(song)} onMouseLeave={() => this.mouseHoverOff(song)}>
+	           				<td>{this.buttonController(song, index)} </td>
+	           				<td>{song.title}</td>
+	           				<td>{song.duration}</td>
+	           			</tr>
+	           		)}
+	           </tbody>
+         	</table>
+		</section>
+  	);
+   }
+ }
 export default Album;
