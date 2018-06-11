79a8ebe9a9c46915295eba9251f5aadf0d47cc38
Paul Brower <pabrower@icloud.com>
2018-06-11 12:44:19 -0700

    completed Bloc Jams checkpoint player bar buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -46,6 +46,15 @@ class Album extends Component {
     }
   }
 
+  handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+    }
+
+
   buttonController(song, index) {
     const pauseButton = <span className="ion-pause"></span>;
     const playButton = <span className="ion-play"></span>;
@@ -97,7 +106,12 @@ class Album extends Component {
             }
           </tbody>
         </table>
-        <PlayerBar isPlaying={this.state.isPlaying} currentSong={this.state.currentSong} />
+        <PlayerBar
+           isPlaying={this.state.isPlaying}
+           currentSong={this.state.currentSong}
+           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+           handlePrevClick={() => this.handlePrevClick()}
+         />
       </section>
     );
   }
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -5,10 +5,10 @@ class PlayerBar extends Component {
 		return (
 			<section className="player-bar">
 				<section id="buttons">
-					<button id="previous">
+					<button id="previous" onClick={this.props.handlePrevClick}>
 						<span className="ion-skip-backward"></span>
 					</button>
-					<button id="play-pause">
+					<button id="play-pause" onClick={this.props.handleSongClick} >
 						<span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>						
 					</button>
 					<button id="next">
